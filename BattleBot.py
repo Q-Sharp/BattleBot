abcdfc0d81e321a63664bef66fc71117b456d86f
Greenfoot5 <greenfoot89@gmail.com>
2020-01-13 23:09:41 +0000

    Removed reference to database in the main file.

--- BattleBot.py
+++ BattleBot.py
@@ -7,34 +7,17 @@ import asyncpg
 import discord
 from discord.ext import commands
 
-"""
-async def run():
-    description = "A bot written in Python that uses asyncpg to connect to a postgreSQL database."
-
-    # NOTE: 127.0.0.1 is the loopback address. If your db is running on the same machine as the code, this address will work
-    credentials = {"user": "BattleBot", "password": "Flaming_Feet7", "database": "BattleBot", "host": "127.0.0.1"}
-    db = await asyncpg.create_pool(**credentials)
-
-    # Example create table code, you'll probably change it to suit you
-    await db.execute("CREATE TABLE IF NOT EXISTS users(id bigint PRIMARY KEY, data text);")
-
-    bot = Bot(db=db)
-    try:
-        await bot.start(config.token)
-    except KeyboardInterrupt:
-        # Make sure to do these steps if you use a command to exit the bot
-        await db.close()
-        await bot.logout()
-"""
 class BattleBot(commands.Bot):
     def __init__(self):
         super().__init__(
             command_prefix=self._get_prefix,
             description="A bot for Battle Legion."
         )
+        # Replace with your own ID if you're testing.
         self.owner_ids = [
             270190067354435584, # Green
             224958921993879552, # Flaming
+            301764235887902727 # QoO
             ]
         self.initial_extensions = ["cogs.owner",
                                    "cogs.links",
